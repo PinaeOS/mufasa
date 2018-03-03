d49a840289b1fafe00cd035ea3eb9e9a405a901f
huiyugeng <interhuiyg@163.com>
2018-03-03 21:10:54 +0800

    Add HttpClient Annotation

---
+++ src/main/java/org/pinae/mufasa/client/annotation/Request.java
@@ -0,0 +1,18 @@
+package org.pinae.mufasa.client.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.pinae.mufasa.client.Http;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Request {
+	String method() default Http.HTTP_GET;
+	
+	String url();
+}
