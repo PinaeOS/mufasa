adda427e78aad32f81f9fb81ccb2665629641b80
huiyugeng <interhuiyg@163.com>
2018-11-04 21:05:00 +0800

    Update JsonDecoder

--- src/main/java/org/pinae/mufasa/client/codec/decoder/JsonDecoder.java
+++ src/main/java/org/pinae/mufasa/client/codec/decoder/JsonDecoder.java
@@ -1,5 +1,18 @@
 package org.pinae.mufasa.client.codec.decoder;
 
-public class JsonDecoder extends AbstractDecoder {
+import java.io.IOException;
 
+import com.alibaba.fastjson.JSON;
+
+public final class JsonDecoder extends AbstractDecoder {
+	public String decode(Object value, Class<?> paramCls) throws IOException {
+		
+		String paramValue = super.decode(value, paramCls);
+		
+		if (paramValue == null) {
+			paramValue = JSON.toJSONString(value);
+		}
+		
+		return paramValue;
+	}
 }
